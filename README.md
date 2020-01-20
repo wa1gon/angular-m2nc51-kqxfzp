36208a7c0be34f95d850d5a25ba3b26f254b8e5b
wa1gon <darryl@wagoner.me>
2020-01-20 16:37:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-m2nc51-kqxfzp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-m2nc51-kqxfzp)
---
+++ app/snack-bar-component-example.ts
@@ -0,0 +1,44 @@
+import { Component, Inject } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig, 
+          MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material';
+
+/**
+ * @title Snack-bar with a custom component
+ */
+@Component({
+  selector: 'snack-bar-component-example',
+  templateUrl: 'snack-bar-component-example.html',
+})
+export class SnackBarComponentExample {
+  configSuccess: MatSnackBarConfig = {
+    panelClass: 'style-success',
+    duration: 10000,
+    horizontalPosition: 'left',
+    verticalPosition: 'bottom'
+  };
+
+  constructor(public snackBar: MatSnackBar) { }
+
+  openSnackBar() {
+    this.snackBar.openFromComponent(PizzaPartyComponent, {
+      data: 'Sample data',
+      ...this.configSuccess
+    });
+  }
+}
+
+
+@Component({
+  selector: 'snack-bar-component-example-snack',
+  templateUrl: 'snack-bar-component-example-snack.html',
+})
+export class PizzaPartyComponent {
+  constructor( 
+    public snackBarRef: MatSnackBarRef<PizzaPartyComponent>,
+    @Inject(MAT_SNACK_BAR_DATA) public data: any) { }
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
